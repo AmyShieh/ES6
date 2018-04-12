a51a8d29adb98fc306924756cf3f34627a8200a5
amyshieh <576535019@qq.com>
2018-04-12 23:31:24 +0800

    babel安装项

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+/**
+ * Created by amyqing719@gmail.com on 12/04/2018.
+ */
+var a = 1;
+console.log(a);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by amyqing719@gmail.com on 12/04/2018.
+ */
+let a = 1;
+console.log(a);
